50fc9560274eeaf7a01e54c73ca681db0ae3221f
Colson <colsoncalifornication@gmail.com>
2024-03-22 11:04:57 +0545

    render lists of songs inside song list component

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -2,12 +2,27 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class SongList extends Component {
+  renderList() {
+    return this.props.songs.map((song) => {
+      return (
+        <div className='item' key={song.title}>
+          <div className='right floated content'>
+            <button className='ui button primary'>Select</button>
+          </div>
+
+          <div className='content'>{song.title}</div>
+        </div>
+      );
+    });
+  }
+
   render() {
     // After mapStateToProps, now props is:
     // this.props === { songs: state.songs }
-    console.log(this.props);
+    // console.log(this.props);
+    // console.log(this.props.songs)
 
-    return <div>SongList</div>;
+    return <div className='ui divided list'>{this.renderList()}</div>;
   }
 }
 
