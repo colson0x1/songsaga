911f2634a0a6b408d441f326c0be57928fe7fed0
Colson <colsoncalifornication@gmail.com>
2024-03-22 11:28:01 +0545

    build a list with redux data: call action creators

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions';
 
 class SongList extends Component {
   renderList() {
@@ -7,7 +8,12 @@ class SongList extends Component {
       return (
         <div className='item' key={song.title}>
           <div className='right floated content'>
-            <button className='ui button primary'>Select</button>
+            <button
+              className='ui button primary'
+              onClick={() => this.props.selectSong(song)}
+            >
+              Select
+            </button>
           </div>
 
           <div className='content'>{song.title}</div>
@@ -21,6 +27,7 @@ class SongList extends Component {
     // this.props === { songs: state.songs }
     // console.log(this.props);
     // console.log(this.props.songs)
+    // console.log(this.props);
 
     return <div className='ui divided list'>{this.renderList()}</div>;
   }
@@ -35,11 +42,22 @@ class SongList extends Component {
 // i.e It's always going to get first argument of state
 // And is always going to return an object that is going to show up as props
 // inside of our Component
+
+// Everytime we click on that select button, this function should rerun and
+// is going to be called with a new state object a second time, third time,
+// or fourth time
+// Anytime we click the button, we should see new console.log and we should
+// see the new selected song printed out inside that state object
 const mapStateToProps = (state) => {
   // console.log(state);
   // return state;
+  console.log(state);
 
   return { songs: state.songs };
 };
 
-export default connect(mapStateToProps)(SongList);
+// Second argument to connect function is an object
+// The connect function is going to take that selectSong action creator
+// and pass it into our Component as a prop
+
+export default connect(mapStateToProps, { selectSong })(SongList);
